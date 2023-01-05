2a33b0171c8fab0a708aea1cd3bae289f808d121
Steve <ycsteven.tsao@gmail.com>
2023-01-05 23:48:37 +0800

    受控組件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
 import './App.css';
 
+class Counter extends React.Component {
+
+  state = {
+    message: 'this is message'
+  };
+
+  inputChange = (e) => {
+    console.log('trigger change event', e.target.value);
+    this.setState({ message: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.message} onChange={this.inputChange}/>
+      </div>);
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Counter/>
     </div>);
 }
 
