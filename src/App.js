0ec1da135f73dd79111873c8743e1dec4c7b49a8
Steve <ycsteven.tsao@gmail.com>
2023-01-06 06:48:41 +0800

    非受控阻件

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { createRef } from "react";
 import './App.css';
 
 class Counter extends React.Component {
 
+  msgRef = createRef();
   state = {
     message: 'this is message'
   };
-
   inputChange = (e) => {
     console.log('trigger change event', e.target.value);
     this.setState({ message: e.target.value });
   };
 
+  getValue = () => {
+    console.log(this.msgRef.current.value);
+  };
+
   render() {
     return (
       <div>
-        <input type="text" value={this.state.message} onChange={this.inputChange}/>
+        <input type="text" value={this.state.message} onChange={this.inputChange} ref={this.msgRef}/>
+        <br/>
+        <input type="text" ref={this.msgRef}/>
+        <button onClick={this.getValue}>get input value</button>
       </div>);
   }
 }
